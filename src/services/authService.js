a5cd0a7c636994304522fb7d5409096d490c0846
Indra <indrabt2403@gmail.com>
2025-06-11 13:22:11 +0530

    Fix API URL to use environment variable

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/auth';
+const API_URL = process.env.REACT_APP_API_URL + '/api/auth';
+
 
 export const register = async (userData) => {
   return axios.post(`${API_URL}/register`, userData);
